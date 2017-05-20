31f4e255374382b942cb2d79df90c4e3ff2924c6
Carmine Di Monaco <carmine.dimonaco@geouniq.com>
2017-05-20 13:43:06 +0200

    Finished user filter

--- common/filters/users.py
+++ common/filters/users.py
@@ -6,6 +6,8 @@ nel service di poter filtrare i risultati in base alle nostre esigenze.
 Il filter con il metodo getConditions(), ritorna un dizionario con le condizioni da noi scelte compatibile con la 
 query diretta a mongodb.
 """
+from bson.objectid import ObjectId
+
 class UserFilter:
     def __init__(self,username=None,password=None,id=None,role=None,email=None):
         self.username=username
@@ -16,5 +18,18 @@ class UserFilter:
 
 
     def getCondition(self):
-        pass
+        filterDict = {}
+        if self.username is not None:
+            filterDict["username"] = self.username
+        if self.password is not None:
+            filterDict["password"] = self.password
+        if self.id is not None:
+            filterDict["_id"] = ObjectId(self.id)
+        if self.role is not None:
+            filterDict["role"] = self.role
+        if self.email is not None:
+            filterDict["email"] = self.email
+
+        return filterDict
+
 
